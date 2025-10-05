7d3145a58a31756f9725968d512faf8392306e55
Nifacy <mai.grishin.alexey@gmail.com>
2025-10-05 14:42:26 +0300

    disable delitem operation for read only extra items

--- mypy/plugins/default.py
+++ mypy/plugins/default.py
@@ -459,7 +459,15 @@ def typed_dict_delitem_callback(ctx: MethodContext) -> Type:
             return AnyType(TypeOfAny.from_error)
 
         for key in keys:
-            if key in ctx.type.required_keys or key in ctx.type.readonly_keys:
+            if (
+                key in ctx.type.required_keys
+                or key in ctx.type.readonly_keys
+                or (
+                    key not in ctx.type.items
+                    and ctx.type.extra_items is not None
+                    and ctx.type.extra_items.is_readonly
+                )
+            ):
                 ctx.api.msg.typeddict_key_cannot_be_deleted(ctx.type, key, key_expr)
             elif key not in ctx.type.items and ctx.type.extra_items is None:
                 ctx.api.msg.typeddict_key_not_found(ctx.type, key, key_expr)
